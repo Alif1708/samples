85620bc7d4048ddb1f9c1b91b1ff7454f07b1c8d
Putraalif00 <putraalif00@gmail.com>
2021-04-14 08:41:09 +0800

    Adding more attributes

---
+++ samples/src/samples/Location.java
@@ -0,0 +1,34 @@
+package samples;
+
+public class Location 
+{
+	 private int age = 21;
+	 private String fName= "Gundi";	 
+	 private String lName= "Ballo";
+	
+	 
+	 public int getAge() {
+		 return age;
+	 }
+	 
+	 public void setAge(int newAge) {
+		 this.age=newAge;	 
+	 }
+	
+	 
+	 public String getFName() {
+		return fName;
+	 }
+
+	 public void setFName(String newFName) {
+		 this.fName=newFName;
+	 }
+	 
+	 public String getLName() {
+		return lName;
+		 }
+
+	 public void setLName(String newLName) {
+		this.lName=newLName;
+	 }
+}
